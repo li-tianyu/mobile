9c674a3ff0ae4b50b908614f404796510d1eb2fc
litianyu <897758347@qq.com>
2019-09-09 18:14:14 +0800

    初始化

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <van-nav-bar
+      title="标题"
+      left-arrow
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="less" scope>
+</style>
